61a89285eca5aa158962b3f470f60902b263d076
srikanta0427 <srikanta0427@gmail.com>
2026-08-21 03:13:39 +0530

    interfaces

--- basics/alias.ts
+++ basics/alias.ts
@@ -1,5 +1,10 @@
 // custom type
 type ID = number | string;
+type isActive = boolean;
+type lengthArr = Array<number>;
 
+// user custom type to everywhere
 const userId : ID = '1234';
+const userAlive : isActive = true;
+const level:lengthArr = [1,2,3,4];
 
